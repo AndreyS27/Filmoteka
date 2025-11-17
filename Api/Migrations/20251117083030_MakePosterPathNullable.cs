cb3045891ea1893b8274ba1c923ba5c04f47032f
Andrey S <salihov.andrey1@yandex.ru>
2025-11-17 11:41:06 +0300

    fix: make PosterPath nullable

---
+++ Api/Migrations/20251117083030_MakePosterPathNullable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakePosterPathNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PosterPath",
+                table: "Films",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "Duration",
+                table: "Films",
+                type: "interval",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Films");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PosterPath",
+                table: "Films",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
--- Api/Models/Film.cs
+++ Api/Models/Film.cs
@@ -5,11 +5,11 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public int Year { get; set; } 
-        TimeSpan Duration { get; set; }
+        public TimeSpan Duration { get; set; }
         public string Country { get; set; } = string.Empty;
         public string Genre { get; set; } = string.Empty;
         public string Director { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public string PosterPath {  get; set; } = string.Empty;
+        public string? PosterPath {  get; set; }
     }
 }
