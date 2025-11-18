517013796e01181e9c0af5b6d588aca34e9a9d5f
Andrey S <salihov.andrey1@yandex.ru>
2025-11-18 15:12:43 +0300

    feat: add IFilmRepository

---
+++ Api/Interfaces/IFilmRepository.cs
@@ -0,0 +1,13 @@
+﻿using Api.Models;
+
+namespace Api.Interfaces
+{
+    public interface IFilmRepository
+    {
+        Task<IEnumerable<Film>> GetAllFilmsAsync();
+        Task<Film?> GetFilmByIdAsync(int id);
+        Task<Film> AddFilmAsync(Film film);
+        Task<Film?> UpdateFilmAsync(int id, Film film);
+        Task <bool> DeleteFilmAsync(int id);
+    }
+}
