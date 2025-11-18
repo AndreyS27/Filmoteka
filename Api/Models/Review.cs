ff280e339f92fe9f4553b28f5ecd62a68a0c945e
Andrey S <salihov.andrey1@yandex.ru>
2025-11-18 11:20:04 +0300

    fix: add rating field for review model

--- Api/Models/Review.cs
+++ Api/Models/Review.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Text { get; set; } = string.Empty;
+        public int Rating { get; set; }
         public int UserId { get; set; }
         public int FilmId { get; set; }
         public User User { get; set; } = null!;
