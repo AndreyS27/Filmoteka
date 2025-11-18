e10297e731fbce7354b21a997e688d11cb7e3d21
Andrey S <salihov.andrey1@yandex.ru>
2025-11-18 15:38:21 +0300

    feat: implement IFilmService

---
+++ Api/Services/FilmService.cs
@@ -0,0 +1,41 @@
+﻿using Api.Interfaces;
+using Api.Models;
+
+namespace Api.Services
+{
+    public class FilmService : IFilmService
+    {
+        private readonly IFilmRepository _repository;
+
+        public FilmService(IFilmRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Film>> GetAllFilmsAsync()
+        {
+            return await _repository.GetAllFilmsAsync();
+        }
+
+        public async Task<Film?> GetFilmByIdAsync(int id)
+        {
+            return await _repository.GetFilmByIdAsync(id);
+        }
+
+        public async Task<Film> AddFilmAsync(Film film)
+        {
+            return await _repository.AddFilmAsync(film);
+        }
+
+        public async Task<Film?> UpdateFilmAsync(int id, Film film)
+        {
+            return await _repository.UpdateFilmAsync(id, film);
+        }
+
+        public async Task<bool> DeleteFilmAsync(int id)
+        {
+            return await _repository.DeleteFilmAsync(id);
+        }
+
+    }
+}
