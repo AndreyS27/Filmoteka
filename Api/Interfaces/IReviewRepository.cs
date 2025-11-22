4c5179317db77bfcd2f679a209ee8e095de4ff08
Andrey S <salihov.andrey1@yandex.ru>
2025-11-22 10:48:25 +0300

    feat: add repository for reviews

---
+++ Api/Interfaces/IReviewRepository.cs
@@ -0,0 +1,14 @@
+﻿using Api.ModelDto;
+using Api.Models;
+
+namespace Api.Interfaces
+{
+    public interface IReviewRepository
+    {
+        Task<IEnumerable<Review>> GetReviewsForFilmAsync(int filmId);
+        Task<Review?> GetReviewByIdAsync(int id);
+        Task<Review> AddReviewAsync(Review review);
+        Task<Review> UpdateReviewAsync(Review review);
+        Task<bool> DeleteReviewAsync(int id);
+    }
+}
