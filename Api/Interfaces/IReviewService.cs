bf008d1d9f807a358b1d047741590d3e9603ee33
Andrey S <salihov.andrey1@yandex.ru>
2025-11-22 11:16:59 +0300

    feat: add review service v1

---
+++ Api/Interfaces/IReviewService.cs
@@ -0,0 +1,13 @@
+﻿using Api.Models;
+
+namespace Api.Interfaces
+{
+    public interface IReviewService
+    {
+        Task<IEnumerable<Review>> GetReviewsForFilmAsync(int filmId);
+        Task<Review?> GetReviewByIdAsync(int id);
+        Task<Review> AddReviewAsync(Review review);
+        Task<Review> UpdateReviewAsync(Review review);
+        Task<bool> DeleteReviewAsync(int id);
+    }
+}
---
+++ Api/Services/ReviewService.cs
@@ -0,0 +1,40 @@
+﻿using Api.Interfaces;
+using Api.Models;
+
+namespace Api.Services
+{
+    public class ReviewService : IReviewService
+    {
+        private readonly IReviewRepository _reposritory;
+
+        public ReviewService(IReviewRepository repository)
+        {
+            _reposritory = repository;
+        }
+
+        public async Task<Review> AddReviewAsync(Review review)
+        {
+            return await _reposritory.AddReviewAsync(review);
+        }
+
+        public async Task<bool> DeleteReviewAsync(int id)
+        {
+            return await _reposritory.DeleteReviewAsync(id);
+        }
+
+        public async Task<Review?> GetReviewByIdAsync(int id)
+        {
+            return await _reposritory.GetReviewByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Review>> GetReviewsForFilmAsync(int filmId)
+        {
+            return await _reposritory.GetReviewsForFilmAsync(filmId);
+        }
+
+        public async Task<Review> UpdateReviewAsync(Review review)
+        {
+            return await _reposritory.UpdateReviewAsync(review);
+        }
+    }
+}
